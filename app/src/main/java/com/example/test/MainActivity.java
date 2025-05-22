0b3592df182dbabd8de3fceb2ef481925eb431b0
Charles <1140601003@qq.com>
2025-05-22 11:03:29 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // 这里填你需要打包的 H5 页面链接
-        webView.loadUrl("http://192.168.1.138:5003/");
+        webView.loadUrl("https://movie.tnanko.top/");
 
         //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
